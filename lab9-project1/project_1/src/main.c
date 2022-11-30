3ffca79ba054c9e4a4fc08e2dfd064a4c6a5b903
hakidaova <xdaova00@vutbr.cz>
2022-11-30 14:57:38 +0100

    mes

--- lab9-project1/project_1/src/main.c
+++ lab9-project1/project_1/src/main.c
@@ -25,6 +25,20 @@
  * 
  **********************************************************************/
 
+/* Defines -----------------------------------------------------------*/
+#define JS_SW PD2   // PD2 is an AVR pin where the button of the joystick is connected
+#define JS_VRx PC0  // PC0 is where the VRx pin of the joystick is connected
+#define JS_VRy PC1  // PC1 is where the VRy pin of the joystick is connected
+#define LED_RED PB5 // PB5 is where the LED diode is connected
+#define EN_SW PB2   // PB2 is an AVR pin where the button of the encoder is connected
+#define EN_DT PB3   // PB3 is where the DT pin of the encoder is connected
+#define EN_CLK PB4  // PB4 is where the CLK pin of the encoder is connected
+
+#define DELAY 250 // Delay in milliseconds
+#ifndef F_CPU
+# define F_CPU 16000000 // CPU frequency in Hz required for delay funcs
+#endif
+
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
@@ -47,56 +61,24 @@ int main(void)
     lcd_init(LCD_DISP_ON_CURSOR_BLINK);
 
     // Put string(s) on LCD screen
+          // putt string "Stopky" to LCD
+    lcd_gotoxy(0,0);
+    lcd_puts("Stopky ");
+
+          // putt string "Budik" to LCD 
+    lcd_gotoxy(0,1);
+    lcd_puts("Budik ");
+
     
-    lcd_gotoxy(0, 0);
-    lcd_puts("Waveform: ");
-
-    lcd_gotoxy(0, 1);
-    lcd_puts(" ");
-
-    lcd_gotoxy(8, 1);
-    lcd_puts("Speed: ");
-
-    // Custom character definition using https://omerk.github.io/lcdchargen/
-    uint8_t customChar[16] = {
-        0b00000,    
-        0b00000,
-        0b01110,
-        0b10001,
-        0b00000,
-        0b00000,
-        0b00000,
-        0b00000,
-
-        0b00000,
-        0b00000,
-        0b00000,
-        0b00000,
-        0b10001,
-        0b01110,
-        0b00000,
-        0b00000
-    };
-
-    lcd_command(1<<LCD_CGRAM);       // Set addressing to CGRAM (Character Generator RAM)
-                                     // ie to individual lines of character patterns
-    for (uint8_t i = 0; i < 24; i++)  // Copy new character patterns line by line to CGRAM
-        lcd_data(customChar[i]);
-    lcd_command(1<<LCD_DDRAM);       // Set addressing back to DDRAM (Display Data RAM)
-                                     // ie to character codes
-
-    // Display symbol with Character code 0
-    lcd_gotoxy(10,0);
-    lcd_putc(0x00);
-    lcd_putc(0x01);
-    
-
     // Configuration of 8-bit Timer/Counter2 for Stopwatch update
     // Set the overflow prescaler to 16 ms and enable interrupt
-    TIM2_overflow_16ms();
-    TIM2_overflow_interrupt_enable();
-    TIM0_overflow_4ms();
-    TIM0_overflow_interrupt_enable();
+
+
+  TIM2_overflow_16ms();
+  TIM2_overflow_interrupt_enable();
+  TIM0_overflow_16ms();
+  TIM0_overflow_interrupt_enable();
+
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -121,107 +103,170 @@ int main(void)
  **********************************************************************/
 ISR(TIMER2_OVF_vect)
 {
-
-
-  /*
     static uint8_t no_of_overflows = 0;
-    static uint8_t tenths = 0;      // Tenths of a second
-    static uint8_t seconds = 0;     // Seconds
-    static uint8_t minutes = 0;     // Minutes
-    char string[2];         // String for converted numbers by itoa()
-
+    static uint8_t tenths = 0;  // Tenths of a second
+    static uint8_t seconds = 0;   
+    static uint8_t minutes = 0;
+    char string[2];             // String for converted numbers by itoa()
 
     no_of_overflows++;
     if (no_of_overflows >= 6)
     {
         // Do this every 6 x 16 ms = 100 ms
         no_of_overflows = 0;
-        tenths++;
+        tenths++;  
 
-        if (tenths > 9)
-        {
-            tenths = 0;
-            seconds++;
+        if(tenths > 9)
+        { tenths = 0;
+          seconds++;
+
             if (seconds > 59)
             {
-                seconds = 00;
-                minutes++;
-            }
-
+              seconds = 0;
+              minutes++;
+            }    
+      
         }
-
-        // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
-
-        itoa(tenths, string, 10);  // Convert decimal value to string
-
-        // Display "00:.tenths"
-        lcd_gotoxy(7, 0);
-        lcd_puts(string);
-
-        lcd_gotoxy(6, 0);
-        lcd_putc('.');
-
-        itoa(seconds, string, 10);  // Convert decimal value to string
-        lcd_gotoxy(4, 0);
-        if (seconds < 10)
-        {
-            lcd_gotoxy(4, 0);
-            lcd_puts("0");
-        }
-        lcd_puts(string);     
-
-        lcd_gotoxy(3, 0);
-        lcd_putc(':');
         
-        itoa(minutes, string, 10);  // Convert decimal value to string
-        lcd_gotoxy(1, 0);
-        if (minutes < 10)
-        {
-            lcd_gotoxy(1, 0);
-            lcd_puts("0");
-        }
-        lcd_puts(string); 
+      itoa(minutes, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(8, 0);
+      if (minutes < 10)
+      { 
+        lcd_putc('0');
+      }
+      lcd_puts(string);
+     
+      lcd_gotoxy(10, 0);
+      lcd_putc(':');
 
-        itoa(pow(seconds,2), string, 10);  // Convert decimal value to string
-        lcd_gotoxy(11, 0);
-        lcd_puts(string);
+      itoa(seconds, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(11, 0);
+      if (seconds < 10)
+      {
+        lcd_putc('0');
+      }
+      lcd_puts(string);
 
-        // if
-        lcd_gotoxy(1, 1);
-        lcd_putc(0xff);
+      lcd_gotoxy(13, 0);
+      lcd_putc('.');
         
-  */
-    
+      itoa(tenths, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(14, 0);
+      lcd_puts(string);
 
 
+    }
+}
 
-    
+/* Variables ---------------------------------------------------------*/
+// Custom character definition using https://omerk.github.io/lcdchargen/
+/*--------------------------------------------------------------------*/
+/**
+ * ISR starts when Timer/Counter0 overflows. Update the progress bar on
+ * LCD display every 16 ms.
+ */
 
-    
-    
-    // Else do nothing and exit the ISR
+ISR(TIMER0_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+    static uint8_t tenths = 9;  // Tenths of a second
+    static uint8_t seconds = 59;   
+    static uint8_t minutes = 59;
+    char string[2];             // String for converted numbers by itoa()
 
+    no_of_overflows++;
+    if (no_of_overflows >= 6)
+    {
+        // Do this every 6 x 16 ms = 100 ms
+        no_of_overflows = 0;
+        tenths--;  
+
+        if(tenths == 0)
+        { tenths = 9;
+          seconds--;
+
+            if (seconds == 0)
+            {
+              seconds = 59;
+              minutes--;
+            }    
+      
+        }
+        
+      itoa(minutes, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(8, 1);
+      if (minutes < 10)
+      { 
+        lcd_putc('0');
+      }
+      lcd_puts(string);
+     
+      lcd_gotoxy(10, 1);
+      lcd_putc(':');
+
+      itoa(seconds, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(11, 1);
+      if (seconds < 10)
+      {
+        lcd_putc('0');
+      }
+      lcd_puts(string);
+
+      lcd_gotoxy(13, 1);
+      lcd_putc('.');
+        
+      itoa(tenths, string, 10);  // Convert decimal value to string
+      lcd_gotoxy(14, 1);
+      lcd_puts(string);
+
+      lcd_gotoxy(6, 1);
+      lcd_putc('0');
+      lcd_gotoxy(6, 1);
+
+      /* Encoder routines -------------------------------------------------*/
+      /**********************************************************************
+      * Function: něco
+      * Purpose:  vymyslíme
+      **********************************************************************/
+      EN_state = GPIO_read(&PIN,EN_SW);
+
+      if(EN_state >= 9)
+      {
+        
+      }
+    }
+
+      
     
 }
 
+/* Joystick routines -------------------------------------------------*/
+/**********************************************************************
+ * Function: Joystick positioning
+ * Purpose:  Moving the cursor according to the joystick position.
+ **********************************************************************/
+// const int SW_pin = 2; // digital pin connected to SW
 /*
-ISR(TIMER0_OVF_vect)
+const int VRx = 0; // analog pin connected to VRx
+const int VRy = 1; // analog pin connected to VRy
+
+void setup() 
 {
-    static uint8_t symbol = 0;
-    static uint8_t position = 0;
-    static uint8_t no_of_overflows = 0;
-    no_of_overflows++;
-    if (no_of_overflows >= 5)
-    {
-        // Do this every 5 x 4 ms = 20 ms
-        lcd_gotoxy(1+position, 1);
-        lcd_putc(symbol);
-        
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
+}
 
-    }
-    // WRITE YOUR CODE HERE
+void loop() 
+{
+  Serial.print("X: ");
+  Serial.print(analogRead(VRx));
+  Serial.print(" Y: ");
+  Serial.print(analogRead(VRy));
+  Serial.print(" Z: ");
+  Serial.println(digitalRead(SW_pin));
+  delay(250);
+}
 */
-    
 
 
-    
